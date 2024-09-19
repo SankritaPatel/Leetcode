0891de074ff84e7b71c2688c88fb448c2d9acbbf
Sankrita Patel <161813782+SankritaPatel@users.noreply.github.com>
2024-09-19 05:49:22 +0000

    [LeetCode Sync] Runtime - 0 ms (100.00%), Memory - 9.8 MB (32.85%)

---
+++ my-folder/0338-counting-bits/solution.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    int countOnes(int n){
+        int count = 0;
+        while(n){
+            n = n&(n-1);
+            count++;
+        }
+        return count;
+    }
+
+    vector<int> countBits(int n) {
+        vector<int>ans;
+        for(int i=0;i<=n;i++){
+            ans.push_back(countOnes(i));
+        }
+        return ans;
+    }
+};
