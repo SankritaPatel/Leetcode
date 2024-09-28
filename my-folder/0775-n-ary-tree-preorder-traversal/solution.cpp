384e8d71dfa37191ac1090a446302dc5969a4858
Sankrita Patel <161813782+SankritaPatel@users.noreply.github.com>
2024-09-28 02:21:18 +0000

    [LeetCode Sync] Runtime - 10 ms (86.22%), Memory - 15.3 MB (53.08%)

---
+++ my-folder/0775-n-ary-tree-preorder-traversal/solution.cpp
@@ -0,0 +1,35 @@
+/*
+// Definition for a Node.
+class Node {
+public:
+    int val;
+    vector<Node*> children;
+
+    Node() {}
+
+    Node(int _val) {
+        val = _val;
+    }
+
+    Node(int _val, vector<Node*> _children) {
+        val = _val;
+        children = _children;
+    }
+};
+*/
+
+class Solution {
+public:
+    vector<int> preorder(Node* root) {
+        vector<int>res;
+        helper(root, res);
+        return res;
+    }
+    void helper(Node* root, vector<int> &res){
+        if(root!=NULL){
+            res.push_back(root->val);
+            for(auto child: root->children)
+                helper(child, res);
+        }
+    }
+};
