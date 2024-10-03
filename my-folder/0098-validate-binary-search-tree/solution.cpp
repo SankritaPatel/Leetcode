450b1a351410d24f37ff0981ac471d3e75a98306
Sankrita Patel <161813782+SankritaPatel@users.noreply.github.com>
2024-10-03 03:00:05 +0000

    [LeetCode Sync] Runtime - 7 ms (76.08%), Memory - 20.4 MB (83.69%)

---
+++ my-folder/0098-validate-binary-search-tree/solution.cpp
@@ -0,0 +1,34 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left),
+ * right(right) {}
+ * };
+ */
+class Solution {
+public:
+    bool isValidBST(TreeNode* root) {
+        return isValidBST(root, nullptr, nullptr);
+    }
+
+private:
+    bool isValidBST(TreeNode* root, TreeNode* min, TreeNode* max) {
+        if (root == nullptr) {
+            return true;
+        }
+        if (min != nullptr && root->val <= min->val) {
+            return false;
+        }
+        if (max != nullptr && root->val >= max->val) {
+            return false;
+        }
+        // Recursively check left and right subtrees with updated min and max
+        return isValidBST(root->left, min, root) && isValidBST(root->right, root, max);
+    }
+};
+
